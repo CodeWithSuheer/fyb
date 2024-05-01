34be56190423fdb6fb4b0f206e52cb15e4e10c91
Suheer Zahid <suheer.work18@gmail.com>
2024-05-01 21:53:53 +0500

    ...

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,11 @@ import Signup from "./auth/Signup";
 import Login from "./auth/Login";
 import NotFound from "./components/NotFound";
 import PrivacyPolicy from "./components/PrivacyPolicy";
+import Contact from "./pages/contact/Contact";
+import About from "./pages/about/About";
+import Products from "./pages/products/Products";
+import Cart from "./pages/cart/Cart";
+import Checkout from "./pages/checkout/Checkout";
 // import Footer from "./components/footer/Footer";
 
 function App() {
@@ -18,13 +23,22 @@ function App() {
         <Routes>
           <Route path="*" element={<NotFound />} />
           <Route path="/" element={<HomePage />} />
-          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+
+          {/* ---------- MAIN ROUTES ---------- */}
+          <Route path="/products" element={<Products />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
 
           {/* ---------- AUTH ROUTES ---------- */}
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
           <Route path="/reset" element={<ResetPass />} />
           <Route path="/forget" element={<ForgetPass />} />
+
+          {/* ---------- OTHER ROUTES ---------- */}
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
         </Routes>
         {/* <Footer /> */}
       </BrowserRouter>
--- frontend/src/pages/home/Skincare.tsx
+++ frontend/src/pages/home/Skincare.tsx
@@ -1,6 +1,85 @@
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import { FaStar } from "react-icons/fa";
+import { useRef, useState } from "react";
 
 const Skincare = () => {
+  const [slidesToShow] = useState(4);
+  const sliderRef = useRef(null);
+
+  const data = [
+    {
+      img: "https://cdn.shopify.com/s/files/1/0852/5099/8550/files/Rectangle_3953.png?v=1714511393",
+      name: "Natural Scrum",
+      category: "Skincare",
+      price: "100",
+    },
+    {
+      img: "https://cdn.shopify.com/s/files/1/0852/5099/8550/files/Rectangle_3953.png?v=1714511393",
+      name: "Natural Scrum",
+      category: "Skincare",
+      price: "100",
+    },
+    {
+      img: "https://cdn.shopify.com/s/files/1/0852/5099/8550/files/Rectangle_3953.png?v=1714511393",
+      name: "Natural Scrum",
+      category: "Skincare",
+      price: "100",
+    },
+    {
+      img: "https://cdn.shopify.com/s/files/1/0852/5099/8550/files/Rectangle_3953.png?v=1714511393",
+      name: "Natural Scrum",
+      category: "Skincare",
+      price: "100",
+    },
+  ];
+
+  const next = () => {
+    if (sliderRef.current) {
+      sliderRef.current.slickNext();
+    }
+  };
+
+  const previous = () => {
+    if (sliderRef.current) {
+      sliderRef.current.slickPrev();
+    }
+  };
+
+  const settings = {
+    infinite: true,
+    slidesToShow: slidesToShow,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    pauseOnHover: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
     <>
       <section className="py-14 sm:py-16 px-5 sm:px-4 xl:px-0">
@@ -15,7 +94,10 @@ const Skincare = () => {
 
             <div className="slider_button flex flex-row">
               {/* left arrow */}
-              <button className="mx-1.5 inline-block rounded-full border text-[#EC72AF] hover:text-white border-[#EC72AF] hover:bg-[#EC72AF] p-2.5 focus:outline-none">
+              <button
+                onClick={previous}
+                className="mx-1.5 inline-block rounded-full border text-[#EC72AF] hover:text-white border-[#EC72AF] hover:bg-[#EC72AF] p-2.5 focus:outline-none"
+              >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
@@ -33,7 +115,10 @@ const Skincare = () => {
               </button>
 
               {/* right arrow */}
-              <button className="mx-1.5 inline-block rounded-full border text-[#EC72AF] hover:text-white border-[#EC72AF] hover:bg-[#EC72AF] p-2.5 focus:outline-none">
+              <button
+                onClick={next}
+                className="mx-1.5 inline-block rounded-full border text-[#EC72AF] hover:text-white border-[#EC72AF] hover:bg-[#EC72AF] p-2.5 focus:outline-none"
+              >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
@@ -54,134 +139,47 @@ const Skincare = () => {
 
           {/* DATA */}
           <div className="data">
-            <div className="mt-8 sm:mt-12 grid grid-cols-2 gap-2 sm:grid-cols-2 lg:grid-cols-4 lg:gap-8">
-              {/* CARD 1 */}
-              <div className="mb-3 relative group w-full pt-5 bg-white border border-gray-400 hover-border-2 hover:border-[#EC72AF] cursor-pointer">
-                <img
-                  className="object-cover w-full h-56"
-                  src="https://cdn.shopify.com/s/files/1/0852/5099/8550/files/Rectangle_3953.png?v=1714511393"
-                  alt="products"
-                />
+            <div className="mt-8 sm:mt-12">
+              <Slider ref={sliderRef} {...settings}>
+                {data.map((data, index) => (
+                  <div key={index} className="mx-5 pb-7">
+                    <div className="mb-3 relative group w-60 pt-5 bg-white border border-gray-400 hover-border-2 hover:border-[#EC72AF] cursor-pointer">
+                      <img
+                        className="object-cover w-full h-56"
+                        src={data.img}
+                        alt="products"
+                      />
 
-                <div className="py-5 text-center">
-                  <h3 className="playfair mb-2 text-lg font-semibold text-gray-800">
-                    Natural Serum
-                  </h3>
+                      <div className="py-5 text-center">
+                        <h3 className="playfair mb-2 text-lg font-semibold text-gray-800">
+                          {data.name}
+                        </h3>
 
-                  {/* STARS */}
-                  <div className="mb-2 flex items-center justify-center gap-1">
-                    <FaStar className="text-[#FFC107]" />
-                    <FaStar className="text-[#FFC107]" />
-                    <FaStar className="text-[#FFC107]" />
-                    <FaStar className="text-[#FFC107]" />
-                    <FaStar className="text-[#FFC107]" />
+                        {/* STARS */}
+                        <div className="mb-2 flex items-center justify-center gap-1">
+                          <FaStar className="text-[#FFC107]" />
+                          <FaStar className="text-[#FFC107]" />
+                          <FaStar className="text-[#FFC107]" />
+                          <FaStar className="text-[#FFC107]" />
+                          <FaStar className="text-[#FFC107]" />
+                        </div>
+
+                        <p className="mb-3 text-md text-gray-500">
+                          ({data.category})
+                        </p>
+
+                        <p className="mb-3 text-xl font-semibold text-black">
+                          ${data.price}
+                        </p>
+
+                        <button className="hidden group-hover:block absolute w-28 sm:w-40 -bottom-5 left-0 right-0 text-sm mx-auto py-3 bg-[#EC72AF] text-white font-semibold">
+                          Shop Now
+                        </button>
+                      </div>
+                    </div>
                   </div>
-
-                  <p className="mb-3 text-md text-gray-500">(Skincare)</p>
-
-                  <p className="mb-3 text-xl font-semibold text-black">$100</p>
-
-                  <button className="hidden group-hover:block absolute w-28 sm:w-40 -bottom-5 left-0 right-0 text-sm mx-auto py-3 bg-[#EC72AF] text-white font-semibold">
-                    Shop Now
-                  </button>
-                </div>
-              </div>
-
-              {/* CARD 2 */}
-              <div className="mb-3 relative group w-full pt-5 bg-white border border-gray-400 hover-border-2 hover:border-[#EC72AF] cursor-pointer">
-                <img
-                  className="object-cover w-full h-56"
-                  src="https://cdn.shopify.com/s/files/1/0852/5099/8550/files/Rectangle_3953.png?v=1714511393"
-                  alt="products"
-                />
-
-                <div className="py-5 text-center">
-                  <h3 className="playfair mb-2 text-lg font-semibold text-gray-800">
-                    Natural Serum
-                  </h3>
-
-                  {/* STARS */}
-                  <div className="mb-2 flex items-center justify-center gap-1">
-                    <FaStar className="text-[#FFC107]" />
-                    <FaStar className="text-[#FFC107]" />
-                    <FaStar className="text-[#FFC107]" />
-                    <FaStar className="text-[#FFC107]" />
-                    <FaStar className="text-[#FFC107]" />
-                  </div>
-
-                  <p className="mb-3 text-md text-gray-500">(Skincare)</p>
-
-                  <p className="mb-3 text-xl font-semibold text-black">$100</p>
-
-                  <button className="hidden group-hover:block absolute w-28 sm:w-40 -bottom-5 left-0 right-0 text-sm mx-auto py-3 bg-[#EC72AF] text-white font-semibold">
-                    Shop Now
-                  </button>
-                </div>
-              </div>
-
-              {/* CARD 3 */}
-              <div className="mb-3 relative group w-full pt-5 bg-white border border-gray-400 hover-border-2 hover:border-[#EC72AF] cursor-pointer">
-                <img
-                  className="object-cover w-full h-56"
-                  src="https://cdn.shopify.com/s/files/1/0852/5099/8550/files/Rectangle_3953.png?v=1714511393"
-                  alt="products"
-                />
-
-                <div className="py-5 text-center">
-                  <h3 className="playfair mb-2 text-lg font-semibold text-gray-800">
-                    Natural Serum
-                  </h3>
-
-                  {/* STARS */}
-                  <div className="mb-2 flex items-center justify-center gap-1">
-                    <FaStar className="text-[#FFC107]" />
-                    <FaStar className="text-[#FFC107]" />
-                    <FaStar className="text-[#FFC107]" />
-                    <FaStar className="text-[#FFC107]" />
-                    <FaStar className="text-[#FFC107]" />
-                  </div>
-
-                  <p className="mb-3 text-md text-gray-500">(Skincare)</p>
-
-                  <p className="mb-3 text-xl font-semibold text-black">$100</p>
-
-                  <button className="hidden group-hover:block absolute w-28 sm:w-40 -bottom-5 left-0 right-0 text-sm mx-auto py-3 bg-[#EC72AF] text-white font-semibold">
-                    Shop Now
-                  </button>
-                </div>
-              </div>
-
-              {/* CARD 4 */}
-              <div className="mb-3 relative group w-full pt-5 bg-white border border-gray-400 hover-border-2 hover:border-[#EC72AF] cursor-pointer">
-                <img
-                  className="object-cover w-full h-56"
-                  src="https://cdn.shopify.com/s/files/1/0852/5099/8550/files/Rectangle_3953.png?v=1714511393"
-                  alt="products"
-                />
-
-                <div className="py-5 text-center">
-                  <h3 className="playfair mb-2 text-lg font-semibold text-gray-800">
-                    Natural Serum
-                  </h3>
-
-                  {/* STARS */}
-                  <div className="mb-2 flex items-center justify-center gap-1">
-                    <FaStar className="text-[#FFC107]" />
-                    <FaStar className="text-[#FFC107]" />
-                    <FaStar className="text-[#FFC107]" />
-                    <FaStar className="text-[#FFC107]" />
-                    <FaStar className="text-[#FFC107]" />
-                  </div>
-
-                  <p className="mb-3 text-md text-gray-500">(Skincare)</p>
-
-                  <p className="mb-3 text-xl font-semibold text-black">$100</p>
-
-                  <button className="hidden group-hover:block absolute w-28 sm:w-40 -bottom-5 left-0 right-0 text-sm mx-auto py-3 bg-[#EC72AF] text-white font-semibold">
-                    Shop Now
-                  </button>
-                </div>
-              </div>
+                ))}
+              </Slider>
             </div>
           </div>
         </div>
